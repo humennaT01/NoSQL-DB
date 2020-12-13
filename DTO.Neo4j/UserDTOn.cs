1c5598e6502b5e8a839f4f8d5f55d67efcd3d488
Tetiana Humenna <tetiana.humenna12@gmail.com>
2020-12-13 16:11:09 +0200

    Added DTO for Neo4j

---
+++ DTO.Neo4j/UserDTOn.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace DTO.Neo4j
+{
+    public class UserDTOn
+    {
+        [JsonProperty(PropertyName = "id")]
+        public int userId { get; set; }
+        [JsonProperty(PropertyName = "name")]
+        public string firstName { get; set; }
+        [JsonProperty(PropertyName = "surname")]
+        public string lastName { get; set; }
+        [JsonProperty(PropertyName = "login")]
+        public string login { get; set; }
+    }
+}
